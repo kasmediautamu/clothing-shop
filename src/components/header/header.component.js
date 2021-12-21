ba77d2a4ccd5c38e9fab5291846155608f1c6da7
kasmediautamu <ivan.khabuyhe1@gmail.com>
2021-12-21 05:34:22 +0300

    adds cart redux

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -3,9 +3,10 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-drop-down/cart-drop-down.component";
 import { auth } from "../../firebase/firebase.utils";
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -27,12 +28,15 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropDown />}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 export default connect(mapStateToProps)(Header);
